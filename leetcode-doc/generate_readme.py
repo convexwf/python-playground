48000c8c40695a78b4521b3fa92c736dfe195bbc
convexwf <convexwf@gmail.com>
2024-05-07 15:06:44 +0800

    feat: update lock tables

--- leetcode-doc/generate_readme.py
+++ leetcode-doc/generate_readme.py
@@ -4,16 +4,18 @@
 # @FileName : generate_readme.py
 # @Author : convexwf@gmail.com
 # @CreateDate : 2024-05-07 10:32
-# @UpdateTime : Todo
+# @UpdateTime : 2024-05-07 14:32
 
 import os
 import re
 
-LEETCODE_ROOT = os.environ.get("LEETCODE_ROOT", "/home/ubuntu/work/convexwf/convex-notes/CS-notes/leetcode")
+LEETCODE_ROOT = os.environ.get("LEETCODE_ROOT", "/leetcode")
 
-DOC_DIR = os.path.join(LEETCODE_ROOT, ".adoc")
+DOC_DIR = os.path.join(LEETCODE_ROOT, ".doc")
+LOCK_DIR = os.path.join(LEETCODE_ROOT, ".lock")
 CPP_DIR = os.path.join(LEETCODE_ROOT, ".cpp")
 
+README_FILE = os.path.join(LEETCODE_ROOT, "README.md")
 
 def extract_doc_files(doc_dir):
     doc_files = os.listdir(doc_dir)
@@ -51,6 +53,8 @@ def extract_doc_files(doc_dir):
             continue
             # raise Exception(f"Error: {problem_id} tags not found")
         problem_tags = [it[2:].strip() for it in lines[tags_idx + 3:]]
+        if len(problem_tags) == 0:
+            continue
 
         doc_list.append([problem_id_int, {
             "id": problem_id_int,
@@ -60,10 +64,54 @@ def extract_doc_files(doc_dir):
             "url": problem_url,
             "doc_path": problem_doc_path,
             "difficulty": problem_difficulty,
-            "tags": problem_tags
+            "tags": problem_tags,
+            "is_lock": False
         }])
     doc_list = sorted(doc_list, key=lambda x: x[0])
-    return [doc[1] for doc in doc_list]
+    return doc_list
+
+def extract_lock_files(doc_dir):
+    doc_files = os.listdir(doc_dir)
+    doc_list = []
+    for doc_file in doc_files:
+        if not doc_file.endswith(".md"):
+            continue
+        with open(os.path.join(doc_dir, doc_file), 'r', encoding='utf-8') as fp:
+            lines = fp.readlines()
+        result = re.match(r'\[([0-9]+)\.(.*)\]\((.*)\)', lines[0][2:].strip())
+        problem_id_int = int(result.group(1))
+        problem_id_str = f"{problem_id_int:04d}"
+        problem_title = result.group(2)
+        problem_identifier = doc_file[:-3]
+        problem_url = result.group(3)
+        problem_doc_path = os.path.join(".lock", doc_file)
+
+        # Extract tags
+        tags_idx = -1
+        for idx, line in enumerate(lines[1:]):
+            if "**标签**" in line:
+                tags_idx = idx
+                break
+        if tags_idx == -1:
+            continue
+            # raise Exception(f"Error: {problem_id} tags not found")
+        problem_tags = [it[2:].strip() for it in lines[tags_idx + 3:]]
+        if len(problem_tags) == 0:
+            continue
+
+        doc_list.append([problem_id_int, {
+            "id": problem_id_int,
+            "id_str": problem_id_str,
+            "title": problem_title,
+            "identifier": problem_identifier,
+            "url": problem_url,
+            "doc_path": problem_doc_path,
+            "difficulty": "",
+            "tags": problem_tags,
+            "is_lock": True
+        }])
+    doc_list = sorted(doc_list, key=lambda x: x[0])
+    return doc_list
 
 def extract_cpp_files(cpp_dir, identifier):
     cpp_file = os.path.join(cpp_dir, f"{identifier}.cpp")
@@ -75,7 +123,10 @@ def generate_markdown_table(doc_list):
     table_header = "| Title | Difficulty | Tags | Solution |\n| --- | --- | --- | --- |\n"
     table_content_list = []
     for doc in doc_list:
-        row = f"| [{doc['id_str']}.{doc['title']}]({doc['doc_path']}) | {doc['difficulty']} | {', '.join(doc['tags'])} | "
+        if not doc["is_lock"]:
+            row = f"| [{doc['id_str']}.{doc['title']}]({doc['doc_path']}) | {doc['difficulty']} | {', '.join(doc['tags'])} | "
+        else:
+            row = f"| {doc['id_str']}.{doc['title']} 🔒 | | {', '.join(doc['tags'])} | "
         cpp_exist, cpp_file = extract_cpp_files(CPP_DIR, doc["identifier"])
         if cpp_exist:
             row += f"[cpp]({cpp_file}) |"
@@ -86,18 +137,20 @@ def generate_markdown_table(doc_list):
 
     table_start_idx = 0
     table_end_idx = 0
-    with open("README.md", 'r', encoding='utf-8') as fp:
+    with open(README_FILE, 'r', encoding='utf-8') as fp:
         lines = fp.readlines()
         for idx, line in enumerate(lines):
             if "<!-- Table Start -->" in line:
                 table_start_idx = idx
             if "<!-- Table End -->" in line:
                 table_end_idx = idx
-    with open("README.md", 'w', encoding='utf-8') as fp:
+    with open(README_FILE, 'w', encoding='utf-8') as fp:
         fp.write("".join(lines[:table_start_idx+1]))
         fp.write(table_content)
         fp.write("".join(lines[table_end_idx:]))
 
 if __name__ == '__main__':
     doc_list = extract_doc_files(DOC_DIR)
-    generate_markdown_table(doc_list)
+    lock_list = extract_lock_files(LOCK_DIR)
+    rows = sorted(doc_list + lock_list, key=lambda x: x[0])
+    generate_markdown_table([it[1] for it in rows])
