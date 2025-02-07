8110a6e6bd011f2b418704689842420120d6ada1
convexwf <convexwf@gmail.com>
2025-02-07 12:40:14 +0800

    add japanese translate by openai

--- new_crawler/economics_parse.py
+++ new_crawler/economics_parse.py
@@ -4,16 +4,27 @@
 # @FileName : new_crawler/economics_parse.py
 # @Author : convexwf@gmail.com
 # @CreateDate : 2025-01-29 17:33
-# @UpdateTime : 2025-01-29 17:33
+# @UpdateTime : 2025-02-07 12:40
 
 import ebooklib
 from ebooklib import epub
 from pyquery import PyQuery as pq
 import datetime
 import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
-def parse_economics_epub(epub_file):
+def parse_economics_epub(epub_file, output_dir="tmp/"):
+    """
+    Parses an EPUB file and extracts chapter information.
+
+    Args:
+        epub_file (str): Path to the EPUB file.
+        output_dir (str): Directory to save the output JSON file.
+    """
     book = epub.read_epub(epub_file)
 
     book_title = book.get_metadata("DC", "title")[0][0]
@@ -33,7 +44,7 @@ def parse_economics_epub(epub_file):
             continue
 
         title_block = doc("span[class='calibre7']")
-        subtitle_block = doc("span[class='calibre_11']")
+        subtitle_block = doc("span[class='calibre_11'], span[class='calibre_12']")
         publish_time_addr_block = doc("span[class='calibre_5']")
         content_block_list = list(doc("p[class='calibre_4']").items())
         if not title_block or not subtitle_block:
@@ -42,9 +53,13 @@ def parse_economics_epub(epub_file):
         subtitle = subtitle_block.text()
         publish_time = publish_time_addr_block.text().split("|")[0].strip()
         publish_date = datetime.datetime.strptime(publish_time, "%b %d, %Y %I:%M %p")
-        chapter_content = "\n\n".join(
-            [content_block.text() for content_block in content_block_list[2:]]
-        ).strip()
+
+        content_list = []
+        for content_block in content_block_list:
+            content_list.append(content_block.text().strip())
+            if content_block.text().endswith("■"):
+                break
+        chapter_content = "\n\n".join(content_list[2:]).strip()
 
         chapter_list.append(
             {
@@ -55,7 +70,8 @@ def parse_economics_epub(epub_file):
                 "category": category_title,
             }
         )
-    with open(f"tmp/{book_title}.json", "w", encoding="utf-8") as f:
+    output_path = os.path.join(output_dir, f"{book_title}.json")
+    with open(output_path, "w+", encoding="utf-8") as f:
         json.dump(
             {
                 "book_title": book_title,
@@ -69,5 +85,19 @@ def parse_economics_epub(epub_file):
 
 
 if __name__ == "__main__":
-    epub_file = "tmp/The Economist 2025-07-05.epub"
-    parse_economics_epub(epub_file)
+    input_dir = os.getenv("ECONOMICS_INPUT_DIR")
+    output_dir = os.getenv("ECONOMICS_OUTPUT_DIR")
+    if not os.path.exists(input_dir):
+        print(f"Input directory does not exist: {input_dir}")
+        exit(1)
+    for idx, filename in enumerate(reversed(os.listdir(input_dir))):
+        if idx >= 10:
+            break
+        if not filename.endswith(".epub"):
+            continue
+        epub_file = os.path.join(input_dir, filename)
+        print(f"Processing file: {epub_file}")
+        parse_economics_epub(epub_file, output_dir=output_dir)
+
+    # epub_file = "The Economist 2025-06-21.epub"
+    # parse_economics_epub(os.path.join(input_dir, epub_file), output_dir=output_dir)
--- openai_test/translate_task.py
+++ openai_test/translate_task.py
@@ -4,7 +4,7 @@
 # @FileName : openai_test/translate_task.py
 # @Author : convexwf@gmail.com
 # @CreateDate : 2025-02-05 22:28
-# @UpdateTime : 2025-02-05 22:28
+# @UpdateTime : 2025-02-07 12:40
 
 import os
 from dotenv import load_dotenv
@@ -13,50 +13,167 @@ import json
 
 load_dotenv()
 
-# 你自定义的对话消息，举例是普通聊天格式
-messages = [
-    {"role": "system", "content": "你是一个帮助用户的助手。"},
+ENGLISH_TOKEN_LIMIT = 3000
+JANPANESE_TOKEN_LIMIT = 3000
+
+init_prompt = [
+    {"role": "system", "content": "你是一个热心而且专业的助手。"},
 ]
 
-if __name__ == "__main__":
-    prompt_path = "input.md"
-    if os.path.exists(prompt_path):
-        with open(prompt_path, "r", encoding="utf-8") as f:
-            content = f.read()
-        messages.append({"role": "user", "content": content})
 
-    json_path = "chapter_list.json"
-    if os.path.exists(json_path):
-        with open(json_path, "r", encoding="utf-8") as f:
-            data = json.load(f)
-        chapter_text = data["chapters"][0]["content"]
-        chapter_split = chapter_text.replace("\n\n", "\n").split("\n")
-        chapter = "\n\n".join(chapter_split[:2])
-
-        messages.append({"role": "user", "content": chapter})
-
-    print("Messages:", messages)
-
+def connect_to_openai(messages):
+    """
+    Connects to OpenAI API using the API key from environment variables.
+    """
     client = OpenAI(
         api_key=os.getenv("API_KEY"),
         base_url="https://chat.cloudapi.vip/v1/",
     )
     response = client.responses.create(
         model="gpt-4o",
-        instructions="你是一个帮助用户的助手。",
         input=messages,
-        # stream=True,
     )
+    # log
+    with open("openai_response.log", "a", encoding="utf-8") as log_file:
+        log_file.write(json.dumps(response.to_dict(), ensure_ascii=False) + "\n")
+    return response.output_text.strip()
 
-    print("Response:", response.output_text)
 
-    # text = ""
-    # for chunk in response:
-    #     if "choices" in chunk:
-    #         delta = chunk["choices"][0]["delta"]
-    #         if "content" in delta:
-    #             text += delta["content"]
+def check_openai_connection():
+    """
+    Checks if the OpenAI API connection is working.
+    """
+    reply = connect_to_openai(
+        messages=init_prompt
+        + [
+            {
+                "role": "user",
+                "content": "Translate this text to Chinese: Hello, how are you?",
+            }
+        ]
+    )
+    print("Reply:", reply)
 
-    # output_file = "output.md"
-    # with open(output_file, "w+", encoding="utf-8") as f:
-    #     f.write(text)
+
+def translate_english_text(text):
+    """
+    Translates English text to Chinese.
+
+    Args:
+        text (str): The English text to translate.
+
+    Returns:
+        str: The translated Chinese text.
+    """
+    messages = init_prompt.copy()
+    english_prompt_path = "enlish_prompt.md"
+    if os.path.exists(english_prompt_path):
+        with open(english_prompt_path, "r", encoding="utf-8") as f:
+            english_prompt = f.read()
+        messages.append({"role": "user", "content": english_prompt})
+
+    text_split = text.replace("\n\n", "\n").split("\n")
+
+    chunk_list = []
+    for line in text_split:
+        if (
+            len(chunk_list) == 0
+            or len(chunk_list[-1]) + len(line) > ENGLISH_TOKEN_LIMIT
+        ):
+            chunk_list.append(line)
+        else:
+            chunk_list[-1] += "\n\n" + line
+
+    translate_result = ""
+    for chunk in chunk_list:
+        print(f"Translating chunk of length {len(chunk)}")
+        chunk_messages = messages + [{"role": "user", "content": chunk}]
+        reply = connect_to_openai(messages=chunk_messages)
+        translate = reply[11:-3].strip()
+        while translate.endswith("to be continued"):
+            translate_result += translate[: -len("to be continued")].strip() + "\n\n"
+            chunk_messages.append({"role": "assistant", "content": reply})
+            chunk_messages.append({"role": "user", "content": "continue"})
+            reply = connect_to_openai(messages=chunk_messages)
+            translate = reply[11:-3].strip()
+        if translate.endswith("fin"):
+            translate = translate[: -len("fin")].strip()
+        translate_result += translate + "\n\n"
+
+    with open("translated_output.md", "w+", encoding="utf-8") as f:
+        f.write(translate_result)
+
+
+def translate_japanese_text(text):
+    """
+    Translates Japanese text to Chinese.
+
+    Args:
+        text (str): The Japanese text to translate.
+
+    Returns:
+        str: The translated Chinese text.
+    """
+    messages = init_prompt.copy()
+    japanese_prompt_path = "japanese_prompt.md"
+    if os.path.exists(japanese_prompt_path):
+        with open(japanese_prompt_path, "r", encoding="utf-8") as f:
+            japanese_prompt = f.read()
+        messages.append({"role": "user", "content": japanese_prompt})
+
+    text_split = text.replace("\n\n", "\n").split("\n")
+    chunk_list = []
+    for line in text_split:
+        if (
+            len(chunk_list) == 0
+            or len(chunk_list[-1]) + len(line) > JANPANESE_TOKEN_LIMIT
+        ):
+            chunk_list.append(line)
+        else:
+            chunk_list[-1] += "\n\n" + line
+
+    translate_result = ""
+    for chunk in chunk_list:
+        print(f"Translating chunk of length {len(chunk)}")
+        chunk_messages = messages + [{"role": "user", "content": chunk}]
+        reply = connect_to_openai(messages=chunk_messages)
+        translate = reply[11:-3].strip()
+        while translate.endswith("to be continued"):
+            translate_result += translate[: -len("to be continued")].strip() + "\n\n"
+            chunk_messages.append({"role": "assistant", "content": reply})
+            chunk_messages.append({"role": "user", "content": "continue"})
+            reply = connect_to_openai(messages=chunk_messages)
+            translate = reply[11:-3].strip()
+        if translate.endswith("fin"):
+            translate = translate[: -len("fin")].strip()
+        translate_result += translate + "\n\n"
+
+    with open("translated_output2.md", "w+", encoding="utf-8") as f:
+        f.write(translate_result)
+
+
+if __name__ == "__main__":
+    # test_path = "tmp/The Economist/The Economist 2025-07-05.json"
+    # if not os.path.exists(test_path):
+    #     print(f"Test file does not exist: {test_path}")
+    #     exit(1)
+    # with open(test_path, "r", encoding="utf-8") as f:
+    #     data = json.load(f)
+    # chapter_text = data["chapters"][70]["content"]
+    # print(f"Chapter text length: {len(chapter_text)}")
+    # print(f"Chapter text: {chapter_text[:100]}...")
+    # translate_english_text(chapter_text)
+
+    # with open("translated_input.md", "w+", encoding="utf-8") as f:
+    #     f.write(chapter_text)
+
+    test_path = "tmp/douban_topic_list.json"
+    if not os.path.exists(test_path):
+        print(f"Test file does not exist: {test_path}")
+        exit(1)
+    with open(test_path, "r", encoding="utf-8") as f:
+        data = json.load(f)
+    topic_text = data["topics"][2]["text"]
+    print(f"Topic text length: {len(topic_text)}")
+    print(f"Topic text: {topic_text[:100]}...")
+    translate_japanese_text(topic_text)
